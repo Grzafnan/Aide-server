e6a7bf9507207dc20712014c5abb510d7e7a49ce
grzafnan <grzafnan@gmail.com>
2023-01-24 02:19:46 +0600

    create add to cart

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,7 @@ const client = require('../configs/mongoClient')
 const router = express.Router();
 
 const Products = client.db('AiedDB').collection('products');
+const Carts = client.db('AiedDB').collection('carts');
 
 router.get('/products', async (req, res) => {
   try {
@@ -18,6 +19,17 @@ router.get('/products', async (req, res) => {
       success: false,
       error: error.message
     })
+  }
+})
+
+
+router.post('/add-cart', async (req, res) => {
+  try {
+    const result = await Carts.insertOne(req.body)
+    res.send({ success: true, data: result })
+  } catch (error) {
+    console.log(error.name, error.message);
+    res.send({ success: false, error: error.message })
   }
 })
 
